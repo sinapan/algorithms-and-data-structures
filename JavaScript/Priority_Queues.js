a092403f4b5bd2a859359cab56876cbbeb06ffa3
Sina Pan <pan.s@outlook.com>
2020-01-19 16:11:28 -0500

    minor comment change

--- JavaScript/Priority_Queues.js
+++ JavaScript/Priority_Queues.js
@@ -68,15 +68,16 @@ class PriorityQueue {
 	}
 }
 
-let ER = new PriorityQueue();
-ER.enqueue('common cold', 5);
-ER.enqueue('gunshot wound', 1);
-ER.enqueue('high fever', 4);
-ER.enqueue('broken arm', 2);
-ER.enqueue('glass in foot', 3);
-console.log(ER.dequeue());
-console.log(ER.dequeue());
-console.log(ER.dequeue());
-console.log(ER.dequeue());
-console.log(ER.dequeue());
-console.log(ER.dequeue());
+// // TEST
+// let ER = new PriorityQueue();
+// ER.enqueue('common cold', 5);
+// ER.enqueue('gunshot wound', 1);
+// ER.enqueue('high fever', 4);
+// ER.enqueue('broken arm', 2);
+// ER.enqueue('glass in foot', 3);
+// console.log(ER.dequeue());
+// console.log(ER.dequeue());
+// console.log(ER.dequeue());
+// console.log(ER.dequeue());
+// console.log(ER.dequeue());
+// console.log(ER.dequeue());
