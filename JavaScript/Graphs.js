e4f4cfc8d2151deba1ec17963b376983234c5bdf
Sina Pan <pan.s@outlook.com>
2020-01-20 16:44:48 -0500

    added graph traversal in JS

--- JavaScript/Graphs.js
+++ JavaScript/Graphs.js
@@ -22,10 +22,69 @@ class Graph {
 		}
 		delete this.adjacencyList[vertex];
 	}
+	depthFirstRecursive(start) {
+		const result = [];
+		const visited = {};
+		const adjacencyList = this.adjacencyList;
+
+		(function dfs(vertex) {
+			if (!vertex) return null;
+			visited[vertex] = true;
+			result.push(vertex);
+			adjacencyList[vertex].forEach((neighbor) => {
+				if (!visited[neighbor]) {
+					return dfs(neighbor);
+				}
+			});
+		})(start);
+
+		return result;
+	}
+	depthFirstIterative(start) {
+		const stack = [ start ];
+		const result = [];
+		const visited = {};
+		let currentVertex;
+
+		visited[start] = true;
+		while (stack.length) {
+			currentVertex = stack.pop();
+			result.push(currentVertex);
+
+			this.adjacencyList[currentVertex].forEach((neighbor) => {
+				if (!visited[neighbor]) {
+					visited[neighbor] = true;
+					stack.push(neighbor);
+				}
+			});
+		}
+		return result;
+	}
+	breadthFirst(start) {
+		const queue = [ start ];
+		const result = [];
+		const visited = {};
+		let currentVertex;
+		visited[start] = true;
+
+		while (queue.length) {
+			currentVertex = queue.shift();
+			result.push(currentVertex);
+
+			this.adjacencyList[currentVertex].forEach((neighbor) => {
+				if (!visited[neighbor]) {
+					visited[neighbor] = true;
+					queue.push(neighbor);
+				}
+			});
+		}
+		return result;
+	}
 }
 
 // // TEST
 // let g = new Graph();
+// #####
 // g.addVertex('Dallas');
 // g.addVertex('Tokyo');
 // g.addVertex('Aspen');
@@ -37,3 +96,17 @@ class Graph {
 // g.addEdge('Hong Kong', 'Dallas');
 // g.addEdge('Los Angeles', 'Hong Kong');
 // g.addEdge('Los Angeles', 'Aspen');
+// #####
+// g.addVertex("A")
+// g.addVertex("B")
+// g.addVertex("C")
+// g.addVertex("D")
+// g.addVertex("E")
+// g.addVertex("F")
+// g.addEdge("A", "B")
+// g.addEdge("A", "C")
+// g.addEdge("B","D")
+// g.addEdge("C","E")
+// g.addEdge("D","E")
+// g.addEdge("D","F")
+// g.addEdge("E","F")
